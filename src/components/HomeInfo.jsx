91e322b63f1527e429de3e98f2d75b060533e259
reuterShawn <reutershawn45@gmail.com>
2024-04-07 20:02:41 -0500

    Fix typo

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -16,7 +16,7 @@ const renderContent = {
   1: (
     <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
       Hi, I am <span className="font-semibold">Shawn</span>
-      <br />A Software Engineer ffrom Minnesota
+      <br />A Software Engineer from Minnesota
     </h1>
   ),
   2: (
